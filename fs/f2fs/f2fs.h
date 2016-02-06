ed1ce2b6faa80f04263437e513ba290c224f3657
engstk <eng.stk@sapo.pt>
2016-02-06 22:00:15 +0000

    f2fs: fix build with new OOS 2.2.0
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1638,10 +1638,12 @@ static inline void f2fs_stop_checkpoint(struct f2fs_sb_info *sbi)
 	sbi->sb->s_flags |= MS_RDONLY;
 }
 
+#ifndef VENDOR_EDIT/*Add by eng.stk*/
 static inline struct inode *file_inode(struct file *f)
 {
 	return f->f_path.dentry->d_inode;
 }
+#endif
 
 static inline bool is_dot_dotdot(const struct qstr *str)
 {
